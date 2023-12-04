f4335567692c9e2e756fe76454a34891bac3ba33
Pierre Tardy <pierre.tardy@renault.com>
2023-12-04 17:14:34 +0100

    day1 part2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,6 +1,6 @@
 use std::{fs::File, io::{BufReader, BufRead}};
 
-fn main() {
+fn main_1() {
     let file = File::open("input.txt").expect("File not found");
     let mut sum:u32 = 0;
     for line in BufReader::new(file).lines() {
@@ -11,4 +11,35 @@ fn main() {
         sum += digit.iter().collect::<String>().parse::<u32>().unwrap();
     }
     println!("Sum: {}", sum);
+}
+
+fn main_2() {
+    let file = File::open("input.txt").expect("File not found");
+    let mut sum:u32 = 0;
+    let searches = vec![("one", 1), ("1", 1), ("two", 2), ("2", 2), ("three", 3), ("3", 3), ("four", 4), ("4", 4), ("five", 5), ("5", 5), ("six", 6), ("6", 6), ("seven", 7), ("7", 7), ("eight", 8), ("8", 8), ("nine", 9), ("9", 9), ("zero", 0), ("0", 0)];
+    for line in BufReader::new(file).lines() {
+        let line = line.unwrap();
+        let mut matches:Vec<(usize, usize)> = Vec::new();
+        for s in searches.iter(){
+            if let Some(idx) = line.find(s.0) {
+                matches.push((idx, s.1));
+            }
+        }
+        matches.sort();
+        let mut lmatches:Vec<(i32, usize)> = Vec::new();
+        for s in searches.iter(){
+            if let Some(idx) = line.rfind(s.0) {
+                lmatches.push((-(idx as i32), s.1));
+            }
+        }
+        lmatches.sort();
+        let digit = matches[0].1.to_string() + &lmatches[0].1.to_string();
+        println!("{:?}", digit);
+        sum += digit.parse::<u32>().unwrap();
+    }
+    println!("Sum: {}", sum);
+}
+
+fn main() {
+    main_2();
 }
