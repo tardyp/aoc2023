da1897ac117081ddb2715ec4b158891278133566
Pierre Tardy <pierre.tardy@renault.com>
2023-12-05 12:21:59 +0100

    part2

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -2,18 +2,18 @@ use itertools::Itertools;
 use std::{
     fs::File,
     io::{BufRead, BufReader},
+    ops::Range,
 };
 
 #[derive(Clone, Debug)]
-struct Range {
-    pub destination_range_start: i64,
-    pub source_range_start: i64,
-    pub range_length: i64,
-    }
+struct TranslationRange {
+    pub translation: i64,
+    pub source_range: Range<i64>,
+}
 #[derive(Clone, Debug)]
 struct Map {
     pub name: String,
-    pub ranges: Vec<Range>,
+    pub ranges: Vec<TranslationRange>,
 }
 
 fn main() {
@@ -26,7 +26,7 @@ fn main() {
         .split(seeds)
         .map(|x| x.parse::<i64>().unwrap())
         .collect::<Vec<i64>>();
-    println!("Seeds: {:?}", seeds);
+    let seeds = seed_ranges_to_seed(seeds);
     lines.next().unwrap().unwrap();
     let mut cur_name = "".to_string();
     let mut cur_ranges = vec![];
@@ -51,40 +51,111 @@ fn main() {
         if line.trim().is_empty() {
             continue;
         }
-        println!("{}", line);
         let (destination_range_start, source_range_start, range_length) = sre
-        .split(&line)
-        .map(|x| x.parse::<i64>().unwrap())
-        .collect_tuple().unwrap();
-        cur_ranges.push(Range {
-            destination_range_start,
-            source_range_start,
-            range_length,
+            .split(&line)
+            .map(|x| x.parse::<i64>().unwrap())
+            .collect_tuple()
+            .unwrap();
+        cur_ranges.push(TranslationRange {
+            translation: destination_range_start - source_range_start,
+            source_range: Range {
+                start: source_range_start,
+                end: source_range_start + range_length,
+            },
         });
-
     }
     let map = Map {
         name: cur_name.clone(),
         ranges: cur_ranges.clone(),
     };
     maps.push(map);
-    println!("Maps: {:#?}", maps);
-
-    let mut translation = seeds.iter().map(|x| (*x, *x)).collect::<Vec<(i64, i64)>>();
+    let mut ranges = seeds.clone();
     for m in maps {
-        for i in 0..translation.len() {
-            for r in m.ranges.iter() {
-                let x = translation[i].0;
-                if x >= r.source_range_start && x < r.source_range_start + r.range_length {
-                    if i==1 {
-                    println!("match! {} {} {}", x, r.source_range_start, r.range_length);}
-                    translation[i].0 = r.destination_range_start + x - r.source_range_start;
+        // first iteration, we split the ranges so that translation ranges don't overlap
+        for tr in m.ranges.iter() {
+            let mut next_ranges: Vec<Range<i64>> = vec![];
+            for ri in ranges {
+                let ri = ri.clone();
+                //[    ] TR
+                //   [] RI
+                if tr.source_range.contains(&ri.start) && tr.source_range.contains(&ri.end) {
+                    next_ranges.push(ri.clone());
+                }
+                //[    ] TR
+                //   [ |   ] RI
+                else if tr.source_range.contains(&ri.start) && !tr.source_range.contains(&ri.end)
+                {
+                    next_ranges.push(Range {
+                        start: ri.start,
+                        end: tr.source_range.end,
+                    });
+                    next_ranges.push(Range {
+                        start: tr.source_range.end,
+                        end: ri.end,
+                    });
+                }
+                //      [    ] TR
+                //   [  |  ] RI
+                else if !tr.source_range.contains(&ri.start) && tr.source_range.contains(&ri.end)
+                {
+                    next_ranges.push(Range {
+                        start: ri.start,
+                        end: tr.source_range.start,
+                    });
+                    next_ranges.push(Range {
+                        start: tr.source_range.start,
+                        end: ri.end,
+                    });
+                }
+                //     [ ] TR
+                //   [ | | ] RI
+                else if ri.contains(&tr.source_range.start) && ri.contains(&tr.source_range.end) {
+                    next_ranges.push(Range {
+                        start: ri.start,
+                        end: tr.source_range.start,
+                    });
+                    next_ranges.push(Range {
+                        start: tr.source_range.start,
+                        end: tr.source_range.end,
+                    });
+                    next_ranges.push(Range {
+                        start: tr.source_range.end,
+                        end: ri.end,
+                    });
+                } else {
+                    next_ranges.push(ri.clone());
+                }
+            }
+            ranges = next_ranges;
+        }
+        let mut next_ranges = vec![];
+        for ri in ranges {
+            let mut found = false;
+            for tr in m.ranges.iter() {
+                if tr.source_range.contains(&ri.start) && tr.source_range.contains(&(ri.end-1)) {
+                    next_ranges.push(Range {
+                        start: ri.start + tr.translation,
+                        end: ri.end + tr.translation,
+                    });
+                    found = true;
                     break;
                 }
             }
+            if !found {
+                next_ranges.push(ri.clone());
+            }
         }
-        println!("translation: {} {:?}", m.name, translation);
+        ranges = next_ranges;
     }
-    translation.sort();
-    println!("nearest location: {:?}", translation[0]);
+    let min = ranges.iter().map(|x| x.start).min().unwrap();
+    println!("min: {min}");
+}
+
+fn seed_ranges_to_seed(seed_ranges: Vec<i64>) -> Vec<Range<i64>> {
+    seed_ranges
+        .iter()
+        .tuple_windows()
+        .step_by(2)
+        .map(|(a, b)| (*a..*a + *b))
+        .collect::<Vec<Range<i64>>>()
 }
