6013bc32dae937aac04cf90ff58bc2fc82a690ef
Pierre Tardy <pierre.tardy@renault.com>
2023-12-13 23:22:05 +0100

    part2

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,10 +1,15 @@
-use itertools::Itertools;
 use core::panic;
+use itertools::Itertools;
 use std::{
     collections::{HashMap, HashSet},
     fs::File,
     io::{BufRead, BufReader},
 };
+fn debug(s: &[bool]) -> String {
+    s.iter()
+        .map(|x| if *x { '#' } else { '.' })
+        .collect::<String>()
+}
 fn main() {
     let file = File::open("input.txt").expect("File not found");
     let sre = regex::Regex::new(r" +").unwrap();
@@ -15,36 +20,61 @@ fn main() {
         if l.is_empty() {
             patterns.push(vec![]);
         } else {
-            patterns.last_mut().unwrap().push(l.chars().collect_vec());
+            patterns
+                .last_mut()
+                .unwrap()
+                .push(l.chars().map(|x| x == '#').collect_vec());
         }
     }
     let h = find_horizontals(&patterns);
     let v = find_verticals(&patterns);
-    println!("h: {h} v: {v} -> {}", v*100+h);
+    println!("h: {h} v: {v} -> {}", v * 100 + h);
 }
-
-fn find_horizontal(pattern: &[Vec<char>]) -> usize {
+fn find_horizontal(pattern: &[Vec<bool>]) -> usize {
+    for y in 0..pattern.len() {
+        for x in 0..pattern[0].len() {
+            let mut p = pattern.to_owned();
+            p[y][x] = !p[y][x];
+            println!("trying -> {x} {y}");
+            let h = find_horizontal_(&p, x);
+            // [.x...c...]
+            // [...c...x]
+            if h > 0 {
+                println!("found smudge at {},{}, {h}, {}", x, y, pattern[0].len());
+                return h;
+            }
+        }
+    }
+    0
+}
+fn find_horizontal_(pattern: &[Vec<bool>], must_include: usize) -> usize {
     let ln = pattern[0].len();
-    for c in 1..ln {
+    let mut min = 1;
+    let mut max = ln - 1;
+    max = (ln + must_include) / 2;
+    min = 1+(must_include) / 2;
+    for c in min..max+1 {
         let mut found = true;
         for l in pattern {
-            let (left, right) = 
-            if c <= ln/2 {
-                (&l[..c],
-                &l[c..(c+c)])
+            let (left, right) = if c <= ln / 2 {
+                (&l[..c], &l[c..(c + c)])
             } else {
-                (&l[(c+c-ln)..c],
-                &l[c..])
+                (&l[(c + c - ln)..c], &l[c..])
             };
-            assert_eq!(left.len(), right.len(), "c:{c} left: {:?}, right: {:?}", left, right);
+            assert_eq!(
+                left.len(),
+                right.len(),
+                "c:{c} left: {:?}, right: {:?}",
+                debug(left),
+                debug(right)
+            );
             let mut right = right.to_owned();
             right.reverse();
-            println!("left: {:?}, right: {:?}, {c}", left, right);
+            println!("{} | {}, {c}", debug(left), debug(&right));
             if left != right {
                 found = false;
                 break;
             } else {
-
             }
         }
         if found {
@@ -53,15 +83,16 @@ fn find_horizontal(pattern: &[Vec<char>]) -> usize {
     }
     0
 }
-fn find_horizontals(patterns: &[Vec<Vec<char>>]) -> usize {
+fn find_horizontals(patterns: &[Vec<Vec<bool>>]) -> usize {
     let mut sum = 0;
     for p in patterns {
         sum += find_horizontal(p);
     }
     sum
 }
-fn find_verticals(patterns: &[Vec<Vec<char>>]) -> usize {
+fn find_verticals(patterns: &[Vec<Vec<bool>>]) -> usize {
     let mut sum = 0;
+    println!("verticals");
     for p in patterns {
         // transpose p
         let mut t = vec![];
@@ -73,7 +104,7 @@ fn find_verticals(patterns: &[Vec<Vec<char>>]) -> usize {
             t.push(l);
         }
         for l in &t {
-            println!("{:?}", l);
+            println!("{}", debug(l));
         }
         println!();
         sum += find_horizontal(&t);
