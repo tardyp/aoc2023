c885f8efb28b4a2f5f2ed5ea404050cf44740500
Pierre Tardy <pierre.tardy@renault.com>
2023-12-16 16:57:35 +0100

    part2

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,10 +1,7 @@
-use core::panic;
-use itertools::Itertools;
 use std::{
-    collections::{HashMap, HashSet},
+    collections::HashSet,
     fs::File,
     io::{BufRead, BufReader},
-    time,
 };
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum Direction {
@@ -26,13 +23,13 @@ impl Direction {
 fn print_map(
     map: &[Vec<char>],
     energized: &HashSet<(i64, i64, Direction)>,
-    X: i64,
-    Y: i64,
+    posx: i64,
+    posy: i64,
     d: Direction,
 ) {
     for (y, row) in map.iter().enumerate() {
         for (x, c) in row.iter().enumerate() {
-            if (x == X as usize) && (y == Y as usize) {
+            if (x == posx as usize) && (y == posy as usize) {
                 //print direction with white background
                 match d {
                     Direction::Left => print!("\x1b[47m\x1b[30m<\x1b[0m"),
@@ -66,7 +63,7 @@ fn trace_beam(
     let mut x = x;
     let mut y = y;
     let mut dir = dir;
-    print_map(map, energized, x, y, dir);
+    // print_map(map, energized, x, y, dir);
     while x >= 0 && y >= 0 && x < map[0].len() as i64 && y < map.len() as i64 {
         if !energized.insert((x, y, dir)){
             break;
@@ -113,20 +110,43 @@ fn trace_beam(
         
     }
 }
+fn trace_beam_and_count(
+    map: &[Vec<char>],
+    x: i64,
+    y: i64,
+    dir: Direction,
+) -> usize{
+    let mut energized: HashSet<(i64, i64, Direction)> = HashSet::new();
+    trace_beam(&map, &mut energized, x, y, dir);
+    energized
+        .iter()
+        .map(|(x, y, _)| (*x, *y))
+        .collect::<HashSet<_>>().iter().count()
 
+}
+fn maxab(a: usize, b: usize) -> usize {
+    if a > b {
+        a
+    } else {
+        println!("{} {}", a, b);
+        b
+    }
+}
 fn main() {
     let file = File::open("input.txt").expect("File not found");
-    let sre = regex::Regex::new(r"[-=]").unwrap();
     let lines = BufReader::new(file).lines();
     let map = lines
         .map(|x| x.unwrap().chars().collect::<Vec<_>>())
         .collect::<Vec<_>>();
-    let mut energized: HashSet<(i64, i64, Direction)> = HashSet::new();
-    trace_beam(&map, &mut energized, 0, 0, Direction::Right);
-    let energized = energized
-        .iter()
-        .map(|(x, y, _)| (*x, *y))
-        .collect::<HashSet<_>>();
-
-    println!("{:?}", energized.iter().count());
+    // for all sides of the map, count the number of energized points
+    let mut max = 0;
+    for x in 0..map[0].len() {
+        max = maxab(max,trace_beam_and_count(&map, x as i64, 0, Direction::Down));
+        max = maxab(max,trace_beam_and_count(&map, x as i64, map.len() as i64 -1, Direction::Up));
+    }
+    for y in 0..map.len() {
+        max = maxab(max,trace_beam_and_count(&map, 0, y as i64, Direction::Right));
+        max = maxab(max,trace_beam_and_count(&map, map[0].len() as i64 -1, y as i64, Direction::Left));
+    }
+    println!("{:?}", max);
 }
