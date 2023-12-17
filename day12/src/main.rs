d729e495c317d4fc62b917a492c5a247d54ea7b1
Pierre Tardy <pierre.tardy@renault.com>
2023-12-17 10:01:22 +0100

    day12 part2

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,63 +1,91 @@
+
 use itertools::Itertools;
-use core::panic;
 use std::{
-    collections::{HashMap, HashSet},
+    collections::HashMap,
     fs::File,
     io::{BufRead, BufReader},
 };
+
 fn main() {
     let file = File::open("input.txt").expect("File not found");
     let sre = regex::Regex::new(r" +").unwrap();
-    let mut lines = BufReader::new(file).lines();
+    let lines = BufReader::new(file).lines();
     let mut sum = 0;
     for line in lines {
         let line = line.unwrap();
         let mut parts = sre.split(&line);
-        let mut data = parts.next().unwrap().chars().collect_vec();
-        let mut neighbors = parts.next().unwrap().split(",").map(|x| x.parse::<usize>().unwrap()).collect_vec();
-        let mut comb = vec![data.clone()];
-        println!("{:?}", data);
-        while comb[0].contains(&'?') {
-            let pos = comb[0].iter().position(|&x| x == '?').unwrap();
-            comb = comb.into_iter().flat_map(|x| {
-                let mut x = x;
-                x[pos] = '.';
-                let mut y = x.clone();
-                y[pos] = '#';
-                vec![x, y]
-            }).collect_vec();
+        let data = parts.next().unwrap().chars().collect_vec();
+        let oneighbors = parts
+            .next()
+            .unwrap()
+            .split(",")
+            .map(|x| x.parse::<usize>().unwrap())
+            .collect_vec();
+        let mut fdata = data.clone();
+        let mut neighbors = oneighbors.clone();
+        for _ in 0..4 {
+            fdata.push('?');
+            fdata.append(&mut data.clone());
+            neighbors.append(&mut oneighbors.clone());
         }
-        sum += comb.into_iter().filter(|x| {
-            let mut count = 0;
-            let mut n = neighbors.clone();
-            n.reverse();
-            for (c,i) in x.iter().enumerate() {
-                match *i {
-                    '.' => {
-                        if count == 0 {
-                            continue;
-                        }
-                        let pop = n.pop().unwrap();
-                        if pop != count {
-                            return false;
-                        }
-                        count = 0;
+
+        let mut suites: HashMap<Vec<usize>, usize> = HashMap::new();
+        suites.insert(vec![0usize], 1);
+        for c in fdata.iter() {
+            match *c {
+                '.' => suites = do_dot(suites, &neighbors),
+                '#' => suites = do_sharp(suites, &neighbors),
+                '?' => {
+                    let mut dot_suite = suites.clone();
+                    dot_suite = do_dot(dot_suite, &neighbors);
+                    suites = do_sharp(suites, &neighbors);
+                    for (k, v) in dot_suite.into_iter() {
+                        *suites.entry(k).or_insert(0) += v;
                     }
-                    '#' => {
-                        if n.is_empty() {
-                            return false;
-                        }
-                        count += 1;
-                    }
-                    _ => {}
                 }
+                _ => (),
             }
-            if n == [count] || n.is_empty() && count == 0 {
-                return true;
+            // println!("{:?} {:?} {:?}", suites, &fdata[i..], neighbors);
+        }
+
+        for (mut x, count) in suites.into_iter() {
+            if x[x.len() - 1] == 0 {
+                x.pop();
             }
-            false
-        }).count();
-        println!("\n\nsum: {}\n\n", sum);
+            if *x == neighbors {
+                sum += count;
+            }
+        }
     }
-    println!("{}", sum);        
+    println!("sum:{}", sum);
+}
+
+fn do_sharp(suites:  HashMap<Vec<usize>, usize>, neighbors: &Vec<usize>) ->  HashMap<Vec<usize>, usize> {
+    let mut next = HashMap::new();
+    for (mut x, count) in suites.into_iter() {
+        if x.len() > neighbors.len() {
+            continue;
+        }
+        let last = x.last_mut().unwrap();
+        *last += 1;
+        if *last <= neighbors[x.len() - 1] {
+            *next.entry(x).or_insert(0) += count;
+        }
+    }
+    next
+}
+
+fn do_dot(suites:  HashMap<Vec<usize>, usize>, neighbors: &Vec<usize>) ->  HashMap<Vec<usize>, usize> {
+    let suites:  HashMap<Vec<usize>, usize> = suites
+        .into_iter()
+        .filter(|(x, _)| x[x.len()-1] == 0 || neighbors.starts_with(x))
+        .collect();
+    let mut next = HashMap::new();
+    for (mut x, count) in suites.into_iter() {
+        if x[x.len() - 1] != 0 {
+            x.push(0);
+        }
+        *next.entry(x).or_insert(0) += count;
+    }
+    next
 }
