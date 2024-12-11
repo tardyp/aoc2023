8dee7d8038ec8a841a6673a618900f18d989a4fd
Pierre Tardy <pierre.tardy@renault.com>
2024-12-11 21:07:51 +0100

    day11 part2

--- 2024/day11/src/main.rs
+++ 2024/day11/src/main.rs
@@ -1,41 +1,69 @@
 use std::fs;
+use std::collections::HashMap;
 
-fn apply_rules(stones: &Vec<u64>) -> Vec<u64> {
-    let mut next_stones = Vec::new();
-    for &stone in stones {
-        if stone == 0 {
-            next_stones.push(1);
-        } else {
-             let num_str = stone.to_string();
-            if num_str.len() % 2 == 0 {
-                let mid = num_str.len() / 2;
-                 let left = num_str[0..mid].parse::<u64>().unwrap();
-                 let right = num_str[mid..].parse::<u64>().unwrap();
-                next_stones.push(left);
-                next_stones.push(right);
-            } else {
-               next_stones.push(stone * 2024);
-            }
-        }
+fn num_digits(mut n: u64) -> usize {
+    if n == 0 {
+        return 1;
     }
-    next_stones
+    let mut count = 0;
+    while n > 0 {
+        n /= 10;
+        count += 1;
+    }
+    count
+}
+
+fn apply_rules_count(counts: &mut HashMap<u64, u64>) {
+    let mut next_counts: HashMap<u64, u64> = HashMap::new();
+    let mut max_value = 0;
+    for (&value, &count) in counts.iter() {
+            if count == 0 {
+                continue;
+            }
+           if value == 0 {
+                 *next_counts.entry(1).or_insert(0) += count;
+                 continue;
+           }
+
+            let num_digs = num_digits(value);
+
+           if num_digs % 2 == 0{
+                 let mut pow10 = 1;
+                for _ in 0..(num_digs/2){
+                    pow10 *= 10;
+                }
+
+               *next_counts.entry(value / pow10).or_insert(0) += count;
+               *next_counts.entry(value % pow10).or_insert(0) += count;
+            } else{
+                *next_counts.entry(value * 2024).or_insert(0) += count;
+            }
+             if value > max_value{
+                max_value = value;
+            }
+
+    }
+    *counts = next_counts;
 }
 
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
-    let start = std::time::Instant::now();
-    let initial_stones: Vec<u64> = input
+    let stones: Vec<u64> = input
         .trim()
         .split_whitespace()
         .map(|s| s.parse().unwrap())
         .collect();
 
-    let mut current_stones = initial_stones;
-    for i in 0..25 {
-        current_stones = apply_rules(&current_stones);
-        println!("{} {} {:?}",start.elapsed().as_secs_f32(), i, current_stones);
-
+    let mut counts: HashMap<u64, u64> = HashMap::new();
+        for num in stones{
+            *counts.entry(num).or_insert(0) += 1;
+        }
+    for _ in 0..75 {
+         apply_rules_count(&mut counts);
     }
-
-    println!("{}", current_stones.len());
+        let mut total_count: u64 = 0;
+    for (_key,value) in counts.iter(){
+          total_count += value;
+        }
+    println!("{}", total_count);
 }
