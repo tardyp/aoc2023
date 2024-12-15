ba118e18bbc81da066d7f46bfdca281e0d6c5d29
Pierre Tardy <pierre.tardy@renault.com>
2024-12-15 15:23:07 +0100

    part2

--- 2024/day15/src/main.rs
+++ 2024/day15/src/main.rs
@@ -1,5 +1,5 @@
-use std::fs;
 
+use std::fs;
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Cell {
     Wall,
@@ -30,17 +30,19 @@ impl Warehouse {
             let mut row = Vec::new();
             for (c, ch) in line.chars().enumerate() {
                 let cell = match ch {
-                    '#' => Cell::Wall,
-                    '.' => Cell::Empty,
-                    'O' => Cell::Box,
+                    '#' => (Cell::Wall, Cell::Wall),
+                    '.' => (Cell::Empty, Cell::Empty),
+                    'O' => (Cell::BoxLeft, Cell::BoxRight),
                     '@' => {
                         robot_row = r;
-                        robot_col = c;
-                        Cell::Robot
+                        robot_col = c*2;
+                        (Cell::Robot, Cell::Empty)
                     }
                     _ => panic!("Invalid char in grid: {}", ch),
                 };
-                row.push(cell);
+                row.push(cell.0);
+                row.push(cell.1);
+
             }
             grid.push(row);
         }
@@ -55,7 +57,69 @@ impl Warehouse {
             cols,
         }
     }
+    fn try_move_box_horizontal(&mut self, r: isize, c: isize, dc: isize) -> bool {
+        // this one, we just need to shift the box to the right or left
+        // its just that there are two kind of boxes, left and right
+        // but they will shift together
+        let new_box_row = r as usize;
+        let mut new_box_col = (c + dc) as usize;
 
+        loop{
+            match self.grid[new_box_row][new_box_col]{
+                Cell::Wall => return false,
+                Cell::Empty => {
+                    // shift all boxes to the right or left
+                    while new_box_col != c as usize{
+                        let prev_box_col = (new_box_col as isize - dc) as usize;
+                        self.grid[new_box_row][new_box_col] = self.grid[new_box_row][prev_box_col];
+                        new_box_col = prev_box_col;
+                    }
+                    return true;
+                }
+                Cell::BoxLeft | Cell::BoxRight => {
+                    new_box_col = (new_box_col as isize + dc) as usize;
+                }
+                Cell::Robot => panic!("Unexpected robot encountered"),
+            }
+        }
+    }
+    fn try_move_box_vertical(&mut self, r: isize, c: isize, dr: isize, doit: bool) -> bool {
+        // this one is more tricky as each pair of boxes will move together
+        // so we will help need to recursively move the boxes one by one, handling all the cases
+        let nr = (r+dr) as usize;
+        let c = if self.grid[r as usize][c as usize] == Cell::BoxLeft {c} else {c-1};
+        let nc = c as usize;
+        // check if the box can move
+        let shall_move = match (self.grid[nr][nc], self.grid[nr][nc+1]) {
+            (Cell::Empty, Cell::Empty) => true,
+            (Cell::BoxLeft, Cell::BoxRight) => {
+                self.try_move_box_vertical(nr as isize, c, dr, doit)
+            }
+            (Cell::BoxRight, Cell::BoxLeft) => {
+                self.try_move_box_vertical(nr as isize, c, dr, doit) && 
+                self.try_move_box_vertical(nr as isize, c+1, dr, doit)
+            }
+            (Cell::BoxRight, Cell::Empty) => {
+                self.try_move_box_vertical(nr as isize, c, dr, doit)
+            }
+            (Cell::Empty, Cell::BoxLeft) => {
+                self.try_move_box_vertical(nr as isize, c+1, dr, doit)
+            }
+
+            _ => false,
+        };
+        if !shall_move {
+            return false;
+        }
+        if doit {
+            self.grid[nr][nc] = self.grid[r as usize][nc];
+            self.grid[nr][nc+1] = self.grid[r as usize][nc+1];
+            self.grid[r as usize][c as usize] = Cell::Empty;
+            self.grid[r as usize][(c+1) as usize] = Cell::Empty;
+        }
+        return true;
+
+    }
     fn move_robot(&mut self, direction: char) {
         let (dr, dc) = match direction {
             '^' => (-1, 0),
@@ -82,35 +146,23 @@ impl Warehouse {
                 self.robot_row = new_robot_row;
                 self.robot_col = new_robot_col;
             }
-            Cell::Box => {
-                let mut new_box_row = (new_robot_row as isize + dr) as usize;
-                let mut new_box_col = (new_robot_col as isize + dc) as usize;
-
-                if new_box_row >= self.rows || new_box_col >= self.cols {
-                    return; // Stay still
-                }
-                while self.grid[new_box_row][new_box_col] == Cell::Box{
-                    new_box_row = (new_box_row as isize + dr) as usize;
-                    new_box_col = (new_box_col as isize + dc) as usize;
-                    if new_box_row >= self.rows || new_box_col >= self.cols {
-                        return; // Stay still
-                    }
-                }
-                match self.grid[new_box_row][new_box_col] {
-                    Cell::Wall => {
-                        //Stay Still
-                    }
-                    Cell::Box => {
-                        panic!("Unexpected box encountered");
-                    }
-                    Cell::Empty => {
+            Cell::BoxLeft | Cell::BoxRight => {
+                if direction == '>' || direction == '<' {
+                    if self.try_move_box_horizontal(new_robot_row as isize, new_robot_col as isize, dc) {
                         self.grid[self.robot_row][self.robot_col] = Cell::Empty;
                         self.grid[new_robot_row][new_robot_col] = Cell::Robot;
-                        self.grid[new_box_row][new_box_col] = Cell::Box;
                         self.robot_row = new_robot_row;
                         self.robot_col = new_robot_col;
                     }
-                    Cell::Robot => panic!("Unexpected robot encountered"),
+                } else {
+                    if self.try_move_box_vertical(new_robot_row as isize, new_robot_col as isize, dr, false) {
+                        // redo the recursion, but this time we will actually move the boxes
+                        self.try_move_box_vertical(new_robot_row as isize, new_robot_col as isize, dr, true);
+                        self.grid[self.robot_row][self.robot_col] = Cell::Empty;
+                        self.grid[new_robot_row][new_robot_col] = Cell::Robot;
+                        self.robot_row = new_robot_row;
+                        self.robot_col = new_robot_col;
+                    }
                 }
             }
             Cell::Robot => panic!("Unexpected robot encountered"),
@@ -122,7 +174,7 @@ impl Warehouse {
         let mut sum = 0;
         for r in 0..self.rows {
             for c in 0..self.cols {
-                if self.grid[r][c] == Cell::Box {
+                if self.grid[r][c] == Cell::BoxLeft {
                     sum += 100 * r + c;
                 }
             }
@@ -135,7 +187,8 @@ impl Warehouse {
                 let ch = match cell {
                     Cell::Wall => '#',
                     Cell::Empty => '.',
-                    Cell::Box => 'O',
+                    Cell::BoxLeft => '[',
+                    Cell::BoxRight => ']',
                     Cell::Robot => '@',
                 };
                 print!("{}", ch);
